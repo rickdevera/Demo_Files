d505060ff273727509899adc047d279be936ac18
Accurics <admin@accurics.com>
2022-07-03 15:45:40 -0500

    Auto Remediation By Accurics

--- main.tf
+++ main.tf
@@ -33,6 +33,14 @@ resource "aws_s3_bucket" "prod_tf_course" {
   versioning {
     enabled = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_default_vpc" "default" {}
