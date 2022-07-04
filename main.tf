3368b0ee25c12e494e3b3565c12f92b3b4d2c66f
Rick Devera <44529212+rickdevera@users.noreply.github.com>
2022-07-04 01:30:11 -0500

    Revert "bugfix/accurics_remediation_42620331464535477 - Auto Generated Pull Request From Accurics"

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "aws_security_group" "prod_web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["172.31.0.0/24"]
+    cidr_blocks = var.whitelist
   }
   ingress {
     from_port   = 443
