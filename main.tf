85404d3bd1c4acacd5d48045a5a6ab85d5b5dab4
Rick Devera <44529212+rickdevera@users.noreply.github.com>
2022-07-03 19:32:29 -0500

    modified cidr_blocks

--- main.tf
+++ main.tf
@@ -68,13 +68,13 @@ resource "aws_security_group" "prod_web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = var.whitelist
+    cidr_blocks = "0.0.0.0/0"
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = var.whitelist
+    cidr_blocks = "0.0.0.0/0"
   }
   egress {
     from_port   = 0
