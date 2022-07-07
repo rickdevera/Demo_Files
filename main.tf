94b3286f6327130f02a52eeae721e8565f7e81cc
Rick Devera <44529212+rickdevera@users.noreply.github.com>
2022-07-07 09:04:06 -0500

    remediated

--- main.tf
+++ main.tf
@@ -89,8 +89,8 @@ resource "aws_security_group" "prod_web" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-//    cidr_blocks = ["172.31.0.0/16"]
+//    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["172.31.0.0/16"]
   }
   egress {
     from_port   = 0
